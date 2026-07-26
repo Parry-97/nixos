9fa5e1b98c62960dc73a7f5843815abc7b9e9dd9
Param Pal Singh <singhparampal97@gmail.com>
2026-07-26 16:20:46 +0200

    update eza config

--- home/shell-profile.nix
+++ home/shell-profile.nix
@@ -4,13 +4,13 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      ls = "${pkgs.eza}/bin/eza --icons always";
+      ls = "${pkgs.eza}/bin/eza --icons auto";
       lg = "${pkgs.lazygit}/bin/lazygit";
-      l = "${pkgs.eza}/bin/eza -lah --icons always";
-      ll = "${pkgs.eza}/bin/eza -l --icons always";
-      la = "${pkgs.eza}/bin/eza -a --icons always";
-      lt = "${pkgs.eza}/bin/eza --tree --icons always";
-      lla = "${pkgs.eza}/bin/eza -la --icons always";
+      l = "${pkgs.eza}/bin/eza -la --icons auto";
+      ll = "${pkgs.eza}/bin/eza -l --icons auto";
+      la = "${pkgs.eza}/bin/eza -a --icons auto";
+      lt = "${pkgs.eza}/bin/eza --tree --icons auto";
+      lla = "${pkgs.eza}/bin/eza -la --icons auto";
     };
   };
 }
